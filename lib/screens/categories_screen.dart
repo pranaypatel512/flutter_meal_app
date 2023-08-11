9f926bbe24231ac429d90bf782eedd711d1a8a1e
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-11 15:41:51 +0530

    Added basic grid listing screen

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/data/dummy_data.dart';
+import 'package:flutter_meal_app/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          for (final category in availableCategories)
+            CategoryItem(category: category)
+        ],
+      ),
+    );
+  }
+}
