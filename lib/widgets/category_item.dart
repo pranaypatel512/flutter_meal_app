a7ac7ffe1fceb4bce5de24e74150f98aa6634d8e
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-11 15:40:59 +0530

    Design caegory item

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        category.color.withOpacity(0.55),
+        category.color.withOpacity(0.9)
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      child: Text(
+        category.title,
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge
+            ?.copyWith(color: Theme.of(context).colorScheme.onBackground),
+      ),
+    );
+  }
+}
