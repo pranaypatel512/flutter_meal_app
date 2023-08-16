25d0f850fb14f6535981b42219752d0c2823a7d4
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-16 17:45:03 +0530

    Created meal details screen

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/models/meals.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  const MealDetailScreen({super.key, required this.meanItem});
+  final Meal meanItem;
+
+  @override
+  Widget build(BuildContext context) {
+    late Widget bodyContent;
+
+    bodyContent = SingleChildScrollView(
+      child: Column(
+        children: [
+          FadeInImage(
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meanItem.imageUrl)),
+          const SizedBox(
+            height: 14,
+          ),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+                fontWeight: FontWeight.bold),
+          ),
+          for (final ind in meanItem.ingredients)
+            Text(
+              ind,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+          const SizedBox(
+            height: 14,
+          ),
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+                fontWeight: FontWeight.bold),
+          ),
+          for (final ind in meanItem.steps)
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12),
+              child: Text(
+                ind,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ),
+        ],
+      ),
+    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(meanItem.title),
+        ),
+        body: bodyContent);
+  }
+}
