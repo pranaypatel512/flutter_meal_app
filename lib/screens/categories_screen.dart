307ce441743ed39adfc286fe24e89bea51e8c7d4
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-22 16:28:24 +0530

    Using tween with `SlideTransition`

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -61,8 +61,11 @@ class _CategoriesScreenState extends State<CategoriesScreen>
           ],
         ),
         builder: (context, child) => SlideTransition(
-              position: _animationController.drive(
-                  Tween(begin: const Offset(0, 0.3), end: const Offset(0, 0))),
+              position:
+                  Tween(begin: const Offset(0, 0.3), end: const Offset(0, 0))
+                      .animate(CurvedAnimation(
+                          parent: _animationController,
+                          curve: Curves.elasticOut)),
               child: child,
             ));
   }
